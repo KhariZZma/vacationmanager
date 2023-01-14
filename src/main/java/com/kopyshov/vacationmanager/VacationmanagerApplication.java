1d8e9a0ef0ef63c58455fe76411e71dee5a05db5
KhariZZma <dkopysh@gmail.com>
2023-01-14 14:38:47 +0300

    Init project

---
+++ src/main/java/com/kopyshov/vacationmanager/VacationmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.kopyshov.vacationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationmanagerApplication.class, args);
+	}
+
+}
